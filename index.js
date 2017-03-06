1a7cae6ed949e47fc5f840f4d5e3607c4083171b
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-06 09:02:55 +0100

    Trafikverket Hello World

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ module.exports = function(bp) {
 
     bp.messenger.sendText(event.user.id, 'Hello, ' + first_name, { typing: true })
   })
+
+
+  bp.hear(/^sj\s/i, (event, next) => { // We use a regex instead of a hardcoded string
+    const first_name = event.user.first_name
+    const text = event.text
+
+    bp.messenger.sendText(event.user.id, `SJ request: [${text}]]`, { typing: true })
+  })
 }
