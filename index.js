aa5007e2c3e30a57a339519eb2cec6500c53c10b
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-06 09:59:37 +0100

    Reply to sj with get departures

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const trafikverket = require('trafikverket')
+const _ = require('lodash')
+
 module.exports = function(bp) {
   bp.middlewares.load()
 
@@ -10,8 +13,23 @@ module.exports = function(bp) {
 
   bp.hear(/^sj\s/i, (event, next) => { // We use a regex instead of a hardcoded string
     const first_name = event.user.first_name
-    const text = event.text
+    const commandTokens = _.chain(event.text)
+      .split(' ')
+      .drop()
+      .value()
 
-    bp.messenger.sendText(event.user.id, `SJ request: [${text}]]`, { typing: true })
+    trafikverket.getDepartures.apply(this, commandTokens).then((departures) => {
+      var departuresString = 
+        _.reduce(departures, (string, departure) => {
+         return string + `Train ${departure.train} departing at ${departure.time} to ${departure.destination}\n`
+        }, "")
+
+      var message = `SJ departures from '${commandTokens[0]}'`
+      if (commandTokens.length > 1) {
+        message += ` to '${commandTokens[1]}'`
+      }
+      message += `:\n${departuresString}`
+      bp.messenger.sendText(event.user.id, message, { typing: true })
+    })
   })
 }
