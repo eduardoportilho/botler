87e5d955e67f2011942c0786ddf87a610d6d5f2b
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-03-10 20:10:13 +0100

    Fix departures loop

--- index.js
+++ index.js
@@ -6,31 +6,19 @@ module.exports = function(bp) {
 
   bp.hear(/hello/i, (event, next) => { // We use a regex instead of a hardcoded string
     const first_name = event.user.first_name
-
     bp.messenger.sendText(event.user.id, 'Hello, ' + first_name, { typing: true })
   })
 
 
   bp.hear(/^sj\s/i, (event, next) => { // We use a regex instead of a hardcoded string
-    const first_name = event.user.first_name
     const commandTokens = _.chain(event.text)
       .split(' ')
       .drop()
       .value()
+    bp.messenger.sendText(event.user.id, getDeparturesMessage(commandTokens), { typing: true })
 
     trafikverket.getDepartures.apply(this, commandTokens).then((departures) => {
-      var message = `SJ departures from '${commandTokens[0]}'`
-      if (commandTokens.length > 1) {
-        message += ` to '${commandTokens[1]}'`
-      }
-      message += ':'
-      bp.messenger.sendText(event.user.id, message, { typing: true })
-
-      var departuresString = _.chain(departures)
-        .filter((departure) => {
-          return !_.isUndefined(departure.time) && !_.isUndefined(departure.destination)
-        })
-        .forEach(departures, (departure) => {
+      departures.forEach((departure) => {
           bp.messenger.sendText(
             event.user.id, 
             `Train ${departure.train} departing at ${departure.time} to ${departure.destination}`,
@@ -39,4 +27,13 @@ module.exports = function(bp) {
         })
     })
   })
+}
+
+function getDeparturesMessage(commandTokens) {
+  var message = `SJ departures from '${commandTokens[0]}'`
+  if (commandTokens.length > 1) {
+    message += ` to '${commandTokens[1]}'`
+  }
+  message += ':'
+  return message
 }
